8acf753e9587045576eaa2fe09a57111e78b393f
Paveen <paveenhuang@gmail.com>
2025-07-21 16:05:31 +0800

    Update mean.py

--- mean.py
+++ mean.py
@@ -14,13 +14,13 @@ def main():
     parser = argparse.ArgumentParser(description="Average hidden states across samples.")
     parser.add_argument("--input_dir", type=str, required=True, help="Path to directory containing positive_*.npy and negative_*.npy")
     parser.add_argument("--output_dir", type=str, required=True, help="Path to save the averaged files")
-    parser.add_argument("--pooling", type=str, required=True, help="Pooling type, last, mean or sum")
+    parser.add_argument("--pooling", type=str, choices=["mean", "last", "sum"], required=True, help="Pooling strategy: mean, last, or sum")
     parser.add_argument("--size", type=str, required=True, help="Model identifier, e.g., 8B")
     args = parser.parse_args()
 
     positive_path = os.path.join(args.input_dir, f"positive_{args.size}_{args.pooling}.npy")
     negative_path = os.path.join(args.input_dir, f"negative_{args.size}_{args.pooling}.npy")
-
+    
     # Load original hidden states
     positive_hidden = np.load(positive_path)  # shape: (240, 32, 4096)
     negative_hidden = np.load(negative_path)
@@ -32,8 +32,8 @@ def main():
     # output
     os.makedirs(args.output_dir, exist_ok=True)
 
-    positive_out = os.path.join(args.output_dir, f"positive_{args.size}.npy")
-    negative_out = os.path.join(args.output_dir, f"negative_{args.size}.npy")
+    positive_out = os.path.join(args.output_dir, f"positive_{args.size}_{args.pooling}.npy")
+    negative_out = os.path.join(args.output_dir, f"negative_{args.size}_{args.pooling}.npy")
     np.save(positive_out, positive_mean)
     np.save(negative_out, negative_mean)
 
